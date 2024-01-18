0f887ca2f103c137e1d06d2312430ccb6e95f0a0
tsujita-yurika <kotoraneko11@gmail.com>
2024-01-18 16:32:52 +0900

    コメント修正

--- src/testjava/test.java
+++ src/testjava/test.java
@@ -4,18 +4,18 @@ public class test {
 
 	public static void main(String[] args) {
 //		cntとsumを初期化
-		int cnt = 0; //⭐️
+		int cnt = 0;
 		int sum = 0;
 //		forループを使って1から10までの数字を順番に取り出し、その数字が偶数かどうかを判定
 		for(int i = 1; i<= 10; i++) {
 			if(i % 2 == 0) {
-//				もし偶数だった場合、sumにその数字を加算し、cntを1増やす
-				cnt++; //⭐️
+//				もし偶数だった場合、cntを1増やしsumにその数字を加算する
+				cnt++;
 				sum += i;
 			}
 		}	
 //		偶数の合計と偶数の個数を出力
-		System.out.println("1～10までの偶数の個数は" + cnt); //⭐️
+		System.out.println("1～10までの偶数の個数は" + cnt);
 		System.out.println("1～10までの偶数の合計は" + sum);
 		}
 	}
