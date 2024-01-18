820028fe3e1c5900dc33629b861b19610dec8e42
tsujita-yurika <kotoraneko11@gmail.com>
2024-01-18 16:30:27 +0900

    1〜10までに偶数が何個あるか、また偶数の合計は幾つになるか表示させる

--- src/testjava/test.java
+++ src/testjava/test.java
@@ -3,27 +3,21 @@ package testjava;
 public class test {
 
 	public static void main(String[] args) {
-////		初期化、繰り返し条件、処理
-//		for(int i=1; i <= 10; i++){
-////			iが偶数である場合
-//			if(i % 2 == 0){
-////				10 8 6 4 2と繰り返し出力されるiにスペースを入れる
-////				printlnにすると出力が縦書きになる
-//				System.out.print(i+" ");
-//				}
-//			}
-////		空の行を出力する。無くてもここでは結果は同じ
-//		System.out.println();
-//		}
-//	}
-		
-		for(int i = 10; i >= 1; i--) {
-			if(i % 2 == 1) {
-				System.out.println(i+" ");
+//		cntとsumを初期化
+		int cnt = 0; //⭐️
+		int sum = 0;
+//		forループを使って1から10までの数字を順番に取り出し、その数字が偶数かどうかを判定
+		for(int i = 1; i<= 10; i++) {
+			if(i % 2 == 0) {
+//				もし偶数だった場合、sumにその数字を加算し、cntを1増やす
+				cnt++; //⭐️
+				sum += i;
 			}
+		}	
+//		偶数の合計と偶数の個数を出力
+		System.out.println("1～10までの偶数の個数は" + cnt); //⭐️
+		System.out.println("1～10までの偶数の合計は" + sum);
 		}
-		System.out.println();
 	}
-}
 
 		
