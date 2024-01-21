d7639043a2b56007df6ccdfca7019bd8b12d9840
tsujita-yurika <kotoraneko11@gmail.com>
2024-01-21 14:17:28 +0900

    練習３

--- src/testjava/test.java
+++ src/testjava/test.java
@@ -4,14 +4,20 @@ public class test {
 
 	public static void main(String[] args) {
 //	1～10を順番に取り出す
-		for(int i =1; i <= 10; i++) {
-//		iを出力
-			System.out.print(i);
+//		for(int i =1; i <= 10; i++) {
+//		iを横並びに出力
+//			System.out.print(i);
 //			10にはカンマを付けない
-				if(i != 10) {
+//				if(i != 10) {
 //				それ以外はカンマを出力
+//					System.out.print(",");
+
+		for(int i = 1; i <= 10; i++) {
+			System.out.print(i);
+				if(i != 10) {
 					System.out.print(",");
-			}	
-		}
-	}
+			}
+		}		
+	}	
 }
+
