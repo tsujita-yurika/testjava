19652fe4daab57527cea791d6964634825835516
tsujita-yurika <kotoraneko11@gmail.com>
2024-01-21 13:53:57 +0900

    1〜10の数字の偶数のみを大きい順に上から縦に並ぶように表示させる

--- src/testjava/test.java
+++ src/testjava/test.java
@@ -3,32 +3,11 @@ package testjava;
 public class test {
 
 	public static void main(String[] args) {
-//		cntとsumを初期化
-//		int cnt = 0;
-//		int sum = 0;
-////		forループを使って1から10までの数字を順番に取り出し、その数字が偶数かどうかを判定
-//		for(int i = 1; i<= 10; i++) {
-//			if(i % 2 == 0) {
-////				もし偶数だった場合、cntを1増やしsumにその数字を加算する
-//				cnt++;
-//				sum += i;
-//			}
-//		}	
-////		偶数の合計と偶数の個数を出力
-//		System.out.println("1～10までの偶数の個数は" + cnt);
-//		System.out.println("1～10までの偶数の合計は" + sum);
-
-		int cnt = 0;
-		int sum = 0;
-			for(int i = 1; i <= 10; i++) {
-				if(i % 2 == 0) {
-					cnt++;
-					sum += i;
-				}
+		for(int i = 10; i >= 1; i--) {
+			if(i % 2 == 0) {
+				System.out.println(i);
 			}
-		System.out.println(cnt);
-		System.out.println(sum);
 		}
-		}
-
+	}
+}
 	
