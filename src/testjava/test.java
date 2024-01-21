1f16263ec63bc32219ac3d03b2f3c129b24d29ad
tsujita-yurika <kotoraneko11@gmail.com>
2024-01-21 13:59:33 +0900

    練習２

--- src/testjava/test.java
+++ src/testjava/test.java
@@ -3,11 +3,16 @@ package testjava;
 public class test {
 
 	public static void main(String[] args) {
-		for(int i = 10; i >= 1; i--) {
+		int cnt = 0;
+		int sum = 0;
+		for(int i = 1; i <= 10; i++) {
 			if(i % 2 == 0) {
-				System.out.println(i);
+				cnt++;
+				sum += i;
 			}
 		}
+		    System.out.println("1～10までの偶数の数は" + cnt + "個");
+				System.out.println("1～10までの偶数の合計は" + sum + "個");
 	}
 }
 	
