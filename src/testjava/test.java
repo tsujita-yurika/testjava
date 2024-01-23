d5a2fe8084df8aa76c55dd1efdcbdf99352ea552
tsujita-yurika <kotoraneko11@gmail.com>
2024-01-23 21:50:37 +0900

    要素数20の配列を宣言し、要素に0から5ずつ加算して代入させる。[0]~[10]の値の奇数のみカンマ区切りで、[11]~[19]の値は偶数をカンマ区切りで表示

--- src/testjava/test.java
+++ src/testjava/test.java
@@ -3,16 +3,22 @@ package testjava;
 public class test {
 
 	public static void main(String[] args) {
-			int cnt = 0;
-			int sum = 0;
-				for(int i = 1; i <= 10; i++) {
-					if(i % 2 == 0) {
-						cnt++;
-						sum += i;
-					}
-				}
-				System.out.println(cnt);
-				System.out.println(sum);
+		int[] arr = new int[20];
+    for (int i = 0; i < arr.length; i++) {
+        arr[i] = i * 5;
+    }
+    String odd = "";
+    String even = "";
+    for (int i = 0; i < arr.length; i++) {
+        if (i <= 10 && arr[i] % 2 != 0) {
+            odd += arr[i] + ",";
+        } else if (i > 10 && arr[i] % 2 == 0) {
+            even += arr[i] + ",";
+        }
+    }
+    System.out.println("奇数 : " + odd.substring(0, odd.length() - 1));
+    System.out.println("偶数 : " + even.substring(0, even.length() - 1));
 	}	
 }
-
+//1～10までの偶数の数は5
+//1～10までの偶数の合計は30
