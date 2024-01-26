86c28a040c8c874285c8c7cb4e15ceac4a629e10
tsujita-yurika <143866084+tsujita-yurika@users.noreply.github.com>
2024-01-26 18:50:56 +0900

    配列scoresを作成

--- src/testjava/test.java
+++ src/testjava/test.java
@@ -3,19 +3,9 @@ package testjava;
 public class test {
 
 	public static void main(String[] args) {
-//	 int 型の配列を宣言し、 3 つの要素を持つ配列を作成
-	   int[] neko = new int[5];
-
-//	  配列のそれぞれの要素に対して数値を代入
-	    neko[0] = 10;
-	    neko[1] = 12;
-	    neko[2] = 20;
-	    neko[3] = 10;
-	    neko[4] = 15;
-
-//	  for 文を使って順に取り出して表示
-	    for (int i = 0 ; i < 5 ; i++){
-	      System.out.println(neko[i]);
+//	 int方の要素を代入できる配列変数scoresを用意
+	   int[] scores;
+//	 int型の要素を5つ作成し、scoresに代入し、配列scoresの完成
+	   scores = new int[5];
 	  }
 	}	
-}
