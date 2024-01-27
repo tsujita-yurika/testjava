b3e565dc8ae6d5a8f0d4646626833724e33e1dd5
tsujita-yurika <143866084+tsujita-yurika@users.noreply.github.com>
2024-01-27 23:21:29 +0900

    宣言と同時に初期化

--- src/testjava/test.java
+++ src/testjava/test.java
@@ -3,9 +3,12 @@ package testjava;
 public class test {
 
 	public static void main(String[] args) {
-//	 int方の要素を代入できる配列変数scoresを用意
-	   int[] scores;
-//	 int型の要素を5つ作成し、scoresに代入し、配列scoresの完成
-	   scores = new int[5];
+//	宣言と同時に初期化
+//	初期化に指定した要素数で配列のサイズも自動的に決まる
+    int[] foo = { 1, 2, 3 };
+
+    System.out.println(foo[0]);
+    System.out.println(foo[1]);
+    System.out.println(foo[2]);
 	  }
 	}	
