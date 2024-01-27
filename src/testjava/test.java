75b7806aa093dd2c51f3f2151e79b9d47c73e893
tsujita-yurika <143866084+tsujita-yurika@users.noreply.github.com>
2024-01-27 23:24:18 +0900

    Arrays.fillですべての要素に同じ値をセット

--- src/testjava/test.java
+++ src/testjava/test.java
@@ -1,11 +1,14 @@
 package testjava;
 
+import java.util.Arrays;
+
 public class test {
 
 	public static void main(String[] args) {
-//	宣言と同時に初期化
-//	初期化に指定した要素数で配列のサイズも自動的に決まる
-    int[] foo = { 1, 2, 3 };
+//	配列のすべての要素に同じ値をセット
+    int[] foo = new int[3];
+
+    Arrays.fill(foo, 5);
 
     System.out.println(foo[0]);
     System.out.println(foo[1]);
