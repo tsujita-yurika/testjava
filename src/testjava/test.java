c97bf3902548b281b9cf0bc418e01c853897f3ce
tsujita-yurika <143866084+tsujita-yurika@users.noreply.github.com>
2024-01-28 09:20:06 +0900

    宣言と同時に値を代入

--- src/testjava/test.java
+++ src/testjava/test.java
@@ -4,20 +4,30 @@ public class test {
 
 	public static void main(String[] args) {
 //	配列を宣言
-		int foo[] = new int[5];
+//	int foo[] = new int[5];
 
 //		要素を代入
-		foo[0] = 1;
-		foo[1] = 2;
-		foo[2] = 3;
-		foo[3] = 4;
-		foo[4] = 5;		
-		
+//		foo[0] = 1;
+//		foo[1] = 2;
+//		foo[2] = 3;
+//		foo[3] = 4;
+//		foo[4] = 5;		
+//		
 		//	出力
+//		System.out.println(foo[0]);
+//		System.out.println(foo[1]);
+//		System.out.println(foo[2]);
+//		System.out.println(foo[3]);
+//		System.out.println(foo[4]);
+		
+//	上記を簡潔に
+		int[] foo = {1, 2, 3, 4, 5};
+
 		System.out.println(foo[0]);
 		System.out.println(foo[1]);
 		System.out.println(foo[2]);
 		System.out.println(foo[3]);
 		System.out.println(foo[4]);
+		
 	  }
 	}	
