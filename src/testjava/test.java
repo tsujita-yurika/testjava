f79fa2d121ccefb1e17b768fef522f37bad0b3c7
tsujita-yurika <143866084+tsujita-yurika@users.noreply.github.com>
2024-01-28 11:39:07 +0900

    lengthで要素数を取得

--- src/testjava/test.java
+++ src/testjava/test.java
@@ -3,31 +3,8 @@ package testjava;
 public class test {
 
 	public static void main(String[] args) {
-//	配列を宣言
-//	int foo[] = new int[5];
-
-//		要素を代入
-//		foo[0] = 1;
-//		foo[1] = 2;
-//		foo[2] = 3;
-//		foo[3] = 4;
-//		foo[4] = 5;		
-//		
-		//	出力
-//		System.out.println(foo[0]);
-//		System.out.println(foo[1]);
-//		System.out.println(foo[2]);
-//		System.out.println(foo[3]);
-//		System.out.println(foo[4]);
-		
-		
-		String[] foo = {"こんにちは", "b", "c", "d", "e"};
-
-		System.out.println(foo[0]);
-		System.out.println(foo[1]);
-		System.out.println(foo[2]);
-		System.out.println(foo[3]);
-		System.out.println(foo[4]);
+		int[] foo = new int[10];
+		System.out.println("配列の要素数：" + foo.length);
 	
 	  }
 	}	
