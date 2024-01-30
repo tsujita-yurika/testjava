b1ade00a83b0e4faf1a9559022f96a6409f6a14f
tsujita-yurika <143866084+tsujita-yurika@users.noreply.github.com>
2024-01-30 11:28:19 +0900

    添え時に対応した情報の利用

--- src/testjava/test.java
+++ src/testjava/test.java
@@ -2,15 +2,23 @@ package testjava;
 
 public class test {
 
+//DNAの記号をランダムに表示する
 	public static void main(String[] args) {
-		int[] scores = {20, 30, 40, 50, 80};
-		int count = 0;
+		int[] seq = new int[10];
 
-		for(int i = 0; i < scores.length; i++) {
-				if(scores[i] >= 50) {
-					count++;
-				}
-			}
-		System.out.println("50点以上の科目の数は" + count);
-	  }
+//	塩基配列をランダムに育成
+		for(int i = 0; i < seq.length; i++) {
+			seq[i] = new java.util.Random().nextInt(4);
+		}
+		
+//	育成した塩基配列の記号を表示
+		for(int i = 0; i < seq.length; i++) {
+			char[] base = {'A', 'T', 'G', 'C'};
+			System.out.print(base[seq[i]] + " ");
+//		★17行目の内容
+//		int baseType = seq[i];	i番目の数字を取得
+//		char baseChar = base[baseType];　数値に対応する記号を取得
+//		System.out.print(baseChar + " ");　記号を画面に表示
+		}
 	}
+}
