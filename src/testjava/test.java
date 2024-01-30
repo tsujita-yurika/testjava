dce781ac2a6ce6a5c846fe992123f2d3fd6021dd
tsujita-yurika <143866084+tsujita-yurika@users.noreply.github.com>
2024-01-30 11:03:05 +0900

    50点以上の科目の数を調べる

--- src/testjava/test.java
+++ src/testjava/test.java
@@ -4,12 +4,13 @@ public class test {
 
 	public static void main(String[] args) {
 		int[] scores = {20, 30, 40, 50, 80};
-		int sum = 0;
+		int count = 0;
+
 		for(int i = 0; i < scores.length; i++) {
-			sum += scores[i];
+				if(scores[i] >= 50) {
+					count++;
+				}
 			}
-		int avg = sum /scores.length;
-		System.out.println("合計点" + sum);
-		System.out.println("平均点" + avg);
+		System.out.println("50点以上の科目の数は" + count);
 	  }
 	}
