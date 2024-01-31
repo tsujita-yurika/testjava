15de35a501391e782b25b98f2f5e5cec38b82542
tsujita-yurika <143866084+tsujita-yurika@users.noreply.github.com>
2024-01-31 10:08:10 +0900

    参照を切る

--- src/testjava/test.java
+++ src/testjava/test.java
@@ -2,12 +2,10 @@ package testjava;
 
 public class test {
 
-//	配列を複数の変数で参照する
-//	arrayBはarrayAと同じ配列を参照する
 	public static void main(String[] args) {
-		boolean judge = true;
-		if (judge == true) {
 			int[] array = {1, 2, 3};
+//		変数にnullを代入。どこも参照していない状態になる
+			array = null;
+			array[0] = 10;
 		}
 	}
-}
