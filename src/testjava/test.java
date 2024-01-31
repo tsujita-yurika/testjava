4a738ce62ff55b90a5c1323931bd2e8042a55de7
tsujita-yurika <143866084+tsujita-yurika@users.noreply.github.com>
2024-01-31 10:05:05 +0900

    変数の消滅と、ガベージコレクションについて

--- src/testjava/test.java
+++ src/testjava/test.java
@@ -5,10 +5,9 @@ public class test {
 //	配列を複数の変数で参照する
 //	arrayBはarrayAと同じ配列を参照する
 	public static void main(String[] args) {
-		int[] arrayA = {1, 2, 3};
-		int[] arrayB;
-		arrayB = arrayA;
-		arrayB[0] = 100;
-		System.out.println(arrayA[0]);
+		boolean judge = true;
+		if (judge == true) {
+			int[] array = {1, 2, 3};
+		}
 	}
 }
