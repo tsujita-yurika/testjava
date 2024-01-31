bb15b4e8e877d3fbf7acfb0e0c0b45ec5f7fd5c7
tsujita-yurika <143866084+tsujita-yurika@users.noreply.github.com>
2024-01-31 09:57:00 +0900

    配列を複数の変数で参照する

--- src/testjava/test.java
+++ src/testjava/test.java
@@ -2,19 +2,13 @@ package testjava;
 
 public class test {
 
+//	配列を複数の変数で参照する
+//	arrayBはarrayAと同じ配列を参照する
 	public static void main(String[] args) {
-//	拡張for文
-		int[]scores = {20, 30, 40, 50, 80};
-		for (int value : scores) {
-			System.out.println(value);
-		}
+		int[] arrayA = {1, 2, 3};
+		int[] arrayB;
+		arrayB = arrayA;
+		arrayB[0] = 100;
+		System.out.println(arrayA[0]);
 	}
 }
-		
-	//従来のfor文
-//		int[] scores = {20, 30, 40, 50, 80};
-//		for (int i = 0; i < scores.length; i++) {
-//			System.out.println(scores[i]);
-//		}
-//	}
-//}
