cc690a2cffe33aeee6c47e8b653a7f69ad6a0871
tsujita-yurika <143866084+tsujita-yurika@users.noreply.github.com>
2024-02-02 08:57:48 +0900

    文字列のlength()

--- src/testjava/test.java
+++ src/testjava/test.java
@@ -3,17 +3,7 @@ package testjava;
 public class test {
 
 	public static void main(String[] args) {
-//		String[] names = {"佐藤","鈴木","高橋"};
-//	  for (int x = 0;x < 3;x++){
-//	  System.out.println("名前は"+names[x]+"です");
-		
-		String[] names = {"佐藤","鈴木","高橋"};
-	  System.out.println("名前は"+names[0]+"です");
-	  names[0] = "木村";
-	  System.out.println("名前は"+names[0]+"です");
-	  System.out.println("名前は"+names[1]+"です");
-	  names[1] = "田中";
-	  System.out.println("名前は"+names[1]+"です");
+		String str = "Javaで開発";
+		System.out.println(str.length());
 		}
 	}
-//}
