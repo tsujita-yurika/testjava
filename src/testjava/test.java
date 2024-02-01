fe0ab348c413e2ffd1b2111b0638849897487979
tsujita-yurika <143866084+tsujita-yurika@users.noreply.github.com>
2024-02-01 16:54:16 +0900

    配列の要素の上書き

--- src/testjava/test.java
+++ src/testjava/test.java
@@ -3,9 +3,14 @@ package testjava;
 public class test {
 
 	public static void main(String[] args) {
-			int[] array = {1, 2, 3};
-//		変数にnullを代入。どこも参照していない状態になる
-			array = null;
-			array[0] = 10;
+//		String[] names = {"佐藤","鈴木","高橋"};
+//	  for (int x = 0;x < 3;x++){
+//	  System.out.println("名前は"+names[x]+"です");
+		
+		String[] names = {"佐藤","鈴木","高橋"};
+	  System.out.println("名前は"+names[0]+"です");
+	  names[0] = "上野";
+	  System.out.println("名前は"+names[0]+"です");
 		}
 	}
+//}
