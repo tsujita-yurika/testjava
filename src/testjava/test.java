d5b12df2d1c2397a62f98d70c6ae14cef7ce3073
tsujita-yurika <143866084+tsujita-yurika@users.noreply.github.com>
2024-02-02 09:48:42 +0900

    親配列と子配列の要素数を表示

--- src/testjava/test.java
+++ src/testjava/test.java
@@ -3,14 +3,10 @@ package testjava;
 public class test {
 
 	public static void main(String[] args) {
-//	2行3列の配列
-		int[][] scores = new int[2][3];
-		scores[0][0] = 40;
-		scores[0][1] = 50;
-		scores[0][2] = 60;
-		scores[1][0] = 80;
-		scores[1][1] = 60;
-		scores[1][2] = 70;
-		System.out.println(scores[1][1]);
+		int[][] scores = {{40, 50, 60}, {80, 60, 70}};
+//	行数
+		System.out.println(scores.length);
+//	最初の行の列数
+		System.out.println(scores[0].length);
 		}
 	}
