68e3ded9c01b15a0940880ede08cf8ca4fd1bbc0
tsujita-yurika <143866084+tsujita-yurika@users.noreply.github.com>
2024-02-02 10:06:18 +0900

    4-2

--- src/testjava/test.java
+++ src/testjava/test.java
@@ -3,9 +3,13 @@ package testjava;
 public class test {
 
 	public static void main(String[] args) {
-		int[] points = new int[4];
-		double[] weights = new double[5];
-		boolean[] answers = new boolean[3];
-		String[] names = new String[3];
+		int[] moneyList = {121902, 8302, 55100};
+		for (int i = 0; i < moneyList.length; i++) {
+			System.out.println(moneyList[i]);
+			}
+//	拡張forループを使って、moneyList配列の全ての要素を順番に出力
+		for (int m:moneyList) {
+			System.out.println(m);
+			}
 		}
 	}
