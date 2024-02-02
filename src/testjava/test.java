67dbcad070810df8bc71e0c1238c728ffa76e7f2
tsujita-yurika <143866084+tsujita-yurika@users.noreply.github.com>
2024-02-02 09:56:39 +0900

    各配列を準備

--- src/testjava/test.java
+++ src/testjava/test.java
@@ -3,10 +3,9 @@ package testjava;
 public class test {
 
 	public static void main(String[] args) {
-		int[][] scores = {{40, 50, 60}, {80, 60, 70}};
-//	行数
-		System.out.println(scores.length);
-//	最初の行の列数
-		System.out.println(scores[0].length);
+		int[] points = new int[4];
+		double[] weights = new double[5];
+		boolean[] answers = new boolean[3];
+		String[] names = new String[3];
 		}
 	}
