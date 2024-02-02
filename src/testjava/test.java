0b655b545360f81a0e518f1cf9a3f54fb7718c2b
tsujita-yurika <143866084+tsujita-yurika@users.noreply.github.com>
2024-02-02 09:05:59 +0900

    2次元配列の利用

--- src/testjava/test.java
+++ src/testjava/test.java
@@ -3,7 +3,14 @@ package testjava;
 public class test {
 
 	public static void main(String[] args) {
-		String str = "Javaで開発";
-		System.out.println(str.length());
+//	2行3列の配列
+		int[][] scores = new int[2][3];
+		scores[0][0] = 40;
+		scores[0][1] = 50;
+		scores[0][2] = 60;
+		scores[1][0] = 80;
+		scores[1][1] = 60;
+		scores[1][2] = 70;
+		System.out.println(scores[1][1]);
 		}
 	}
