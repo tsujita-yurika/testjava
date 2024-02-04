3271e5ff43f1b931dc7cae7d23f83cfda93c681b
tsujita-yurika <143866084+tsujita-yurika@users.noreply.github.com>
2024-02-04 11:26:34 +0900

    拡張for文

--- src/testjava/test.java
+++ src/testjava/test.java
@@ -4,9 +4,6 @@ public class test {
 
 	public static void main(String[] args) {
 		int[] moneyList = {121902, 8302, 55100};
-			for (int i = 0; i < moneyList.length; i++) {
-				System.out.println(moneyList[i]);
-			}
 			for(int m : moneyList) {
 				System.out.println(m);
 			}
