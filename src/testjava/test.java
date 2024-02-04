2a65d6d31712da30b9a80ad07d909875b21caa5d
tsujita-yurika <143866084+tsujita-yurika@users.noreply.github.com>
2024-02-04 11:26:02 +0900

    for文、拡張for文

--- src/testjava/test.java
+++ src/testjava/test.java
@@ -4,12 +4,11 @@ public class test {
 
 	public static void main(String[] args) {
 		int[] moneyList = {121902, 8302, 55100};
-		for (int i = 0; i < moneyList.length; i++) {
-			System.out.println(moneyList[i]);
+			for (int i = 0; i < moneyList.length; i++) {
+				System.out.println(moneyList[i]);
 			}
-//	拡張forループを使って、moneyList配列の全ての要素を順番に出力
-		for (int m:moneyList) {
-			System.out.println(m);
+			for(int m : moneyList) {
+				System.out.println(m);
 			}
 		}
 	}
