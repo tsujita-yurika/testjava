feeebb0328685e643d636ea5bf7229627390d387
tsujita-yurika <143866084+tsujita-yurika@users.noreply.github.com>
2024-02-06 14:32:41 +0900

    多次元配列

--- src/testjava/test.java
+++ src/testjava/test.java
@@ -3,9 +3,25 @@ package testjava;
 public class test {
 
 	public static void main(String[] args) {
-		int[] moneyList = {0, 1, 2, 3, 4, 5};
-			for(int m : moneyList) {
-				System.out.println(m);
+//	int 型のデータを格納する配列を格納する 2 次元配列を作成
+    int[][] num = new int[2][];
+
+//  要素に対してそれぞれ値を格納
+    num[0] = new int[3];
+    num[0][0] = 78;
+    num[0][1] = 64;
+    num[0][2] = 59;
+
+    num[1] = new int[3];
+    num[1][0] = 58;
+    num[1][1] = 92;
+    num[1][2] = 82;
+    
+//  すべての要素の値を順番に参照して出力
+    for (int i = 0; i < 2; i++){
+      for (int j = 0; j < 3; j++){
+        System.out.println("num[" + i + "][" + j + "] = " + num[i][j]);
 			}
 		}
 	}
+}
