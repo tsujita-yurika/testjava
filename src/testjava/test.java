c9b666839c63e89d89ad44f63e762c1b3a2973e9
tsujita-yurika <143866084+tsujita-yurika@users.noreply.github.com>
2024-02-07 13:49:42 +0900

    メソッドの呼び出し

--- src/testjava/test.java
+++ src/testjava/test.java
@@ -3,21 +3,11 @@ package testjava;
 public class test {
 
 	public static void main(String[] args) {
-//		配列の準備
-		int [] numbers = {3, 4, 9};
-		
-//		メッセージの表示
-		System.out.println("１桁の数字を入力してください");
-		
-//		キーボードからの数字入力
-		int input = new java.util.Scanner(System.in).nextInt();
-		
-//		配列を回しながら判定
-		for(int n : numbers) {
-			if(n == input) {
-				System.out.println("アタリ！");
-
-			}
-		}
+		System.out.println("メソッドを呼び出します");
+		hello();
+		System.out.println("メソッドの呼び出しが終わりました");
+	}
+	public static void hello() {
+		System.out.println("湊さん、こんにちは");
 	}
 }
