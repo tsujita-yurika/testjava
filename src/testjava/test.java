8b1d0b9def9780919836e0e6b2c2c360383acda9
tsujita-yurika <143866084+tsujita-yurika@users.noreply.github.com>
2024-02-08 12:00:41 +0900

    引数の利用

--- src/testjava/test.java
+++ src/testjava/test.java
@@ -4,10 +4,14 @@ public class test {
 
 	public static void main(String[] args) {
 		System.out.println("メソッドを呼び出します");
-		hello();
-		System.out.println("メソッドの呼び出しが終わりました");
+		
+//		名前を渡してhelloメソッドを呼び出す
+		hello("湊");
+		hello("朝香");
+		hello("菅原");
+		System.out.println("メソッドの呼び出しが終りました");
 	}
-	public static void hello() {
-		System.out.println("湊さん、こんにちは");
+	public static void hello(String name) {
+		System.out.println(name + "さん、こんにちは");
 	}
 }
