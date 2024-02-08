f1f81ebac20fc2007eee120d974652352d613288
tsujita-yurika <143866084+tsujita-yurika@users.noreply.github.com>
2024-02-08 12:11:02 +0900

    渡す値が複数の場合のイメージ

--- src/testjava/test.java
+++ src/testjava/test.java
@@ -3,15 +3,11 @@ package testjava;
 public class test {
 
 	public static void main(String[] args) {
-		System.out.println("メソッドを呼び出します");
-		
-//		名前を渡してhelloメソッドを呼び出す
-		hello("湊");
-		hello("朝香");
-		hello("菅原");
-		System.out.println("メソッドの呼び出しが終りました");
+		add(100, 20);
+		add(200, 50);
 	}
-	public static void hello(String name) {
-		System.out.println(name + "さん、こんにちは");
+	public static void add(int x, int y) {
+		int ans = x + y;
+		System.out.println(x + "+" + y "=" + ans);
 	}
 }
