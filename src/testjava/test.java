c5ff427f669343f784ca0c53bf406c3b436e05b4
tsujita-yurika <143866084+tsujita-yurika@users.noreply.github.com>
2024-02-08 12:42:38 +0900

    オーバーロード（引数の型が異なる場合）

--- src/testjava/test.java
+++ src/testjava/test.java
@@ -2,11 +2,27 @@ package testjava;
 
 public class test {
 
+//	1つ目のaddメソッド
 	public static int add(int x, int y) {
-		int ans = x + y;
-		return ans;
+		return x + y;
 	}
+//　２つ目のaddメソッド
+	public static double add(double x, double y) {
+		return x + y;
+	}
+//　３つ目のaddメソッド
+	public static String add(String x, String y) {
+		return x + y;
+	}
+	
 	public static void main(String[] args) {
-		System.out.println(add(add(10, 20), add(30, 40)));
+//	1つ目のaddメソッドが呼び出される
+		System.out.println(add(10, 20));
+//　２つ目のaddメソッドが呼び出される
+		System.out.println(add(3.5, 2.7));
+//　３つ目のaddメソッドが呼び出される
+		System.out.println(add("Hello", "World"));
 	}
+	
 }
+	
