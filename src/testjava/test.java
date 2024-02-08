5f804bffbc57caedffdfe119f7fcbb711b4309be
tsujita-yurika <143866084+tsujita-yurika@users.noreply.github.com>
2024-02-08 12:47:28 +0900

    オーバーロード（引数の値が異なる場合）

--- src/testjava/test.java
+++ src/testjava/test.java
@@ -7,22 +7,13 @@ public class test {
 		return x + y;
 	}
 //　２つ目のaddメソッド
-	public static double add(double x, double y) {
-		return x + y;
+	public static int add(int x, int y, int z) {
+		return x + y + z;
 	}
 //　３つ目のaddメソッド
-	public static String add(String x, String y) {
-		return x + y;
-	}
-	
 	public static void main(String[] args) {
-//	1つ目のaddメソッドが呼び出される
-		System.out.println(add(10, 20));
-//　２つ目のaddメソッドが呼び出される
-		System.out.println(add(3.5, 2.7));
-//　３つ目のaddメソッドが呼び出される
-		System.out.println(add("Hello", "World"));
+		System.out.println("10 + 20 = " + add(10, 20));
+		System.out.println("10 + 20 + 30 = " + add(10, 20, 30));
 	}
-	
 }
 	
