8c2cb17cd2048139a4205efa96808bd83bbf6d3f
tsujita-yurika <143866084+tsujita-yurika@users.noreply.github.com>
2024-02-09 18:27:55 +0900

    戻り値として配列を返す

--- src/testjava/test.java
+++ src/testjava/test.java
@@ -2,18 +2,20 @@ package testjava;
 
 public class test {
 
-//	1つ目のaddメソッド
-	public static int add(int x, int y) {
-		return x + y;
+	public static void main(String args[]){
+	  int data[];
+
+	  data = test();
+	  System.out.println(data[0]);
+	  System.out.println(data[1]);
 	}
-//　２つ目のaddメソッド
-	public static int add(int x, int y, int z) {
-		return x + y + z;
-	}
-//　３つ目のaddメソッド
-	public static void main(String[] args) {
-		System.out.println("10 + 20 = " + add(10, 20));
-		System.out.println("10 + 20 + 30 = " + add(10, 20, 30));
+
+	private static int[] test(){
+	  int data[] = new int[2];
+	  data[0] = 10;
+	  data[1] = 19;
+
+	  return data;
 	}
 }
 	
