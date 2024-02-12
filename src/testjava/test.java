64afdfdfe2b03f24909ea7cea3ba6dbe5531b82a
tsujita-yurika <143866084+tsujita-yurika@users.noreply.github.com>
2024-02-12 12:52:16 +0900

    引数に配列を用いる

--- src/testjava/test.java
+++ src/testjava/test.java
@@ -2,11 +2,13 @@ package testjava;
 
 public class test {
 
-	public static void main(String args[]){
-		String original = "Hello";
-		String msg = new String(original);
-
-		System.out.println(msg);  // Hello
+	public static void printArray(int[] array) { //引数に配列型を指定
+		for (int element : array) {
+			System.out.println(element);
+		}
+	}
+	public static void main(String[] args) {
+		int[] array = {1, 2, 3};
+		printArray(array); //配列を渡す
 	}
 }
-	
