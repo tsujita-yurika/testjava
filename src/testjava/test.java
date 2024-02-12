c4684c64d23ad223ea24570b35fc51ae957dd592
tsujita-yurika <143866084+tsujita-yurika@users.noreply.github.com>
2024-02-12 14:09:52 +0900

    挨拶

--- src/testjava/test.java
+++ src/testjava/test.java
@@ -3,16 +3,16 @@ package testjava;
 public class test {
 
   public static void main(String[] args){
-    test dentaku = new test();
-    dentaku.plus(10,5);
-    dentaku.minus(9,2);
+  	test robot = new test();
+  	robot.hello();
+  	robot.bye();
   }
-
-  void plus(int val1, int val2){
-    System.out.println(val1 + val2);
+  
+  void hello() {
+  	System.out.println("Hello");
   }
-
-  void minus(int val1, int val2){
-    System.out.println(val1 - val2);
+  
+  void bye() {
+  	System.out.println("Bye");
   }
 }
