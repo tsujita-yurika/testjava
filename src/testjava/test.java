c09f389ee303613916718696a460ff6fbd1f2c90
tsujita-yurika <143866084+tsujita-yurika@users.noreply.github.com>
2024-02-13 19:08:15 +0900

    戻り値に配列を使用

--- src/testjava/test.java
+++ src/testjava/test.java
@@ -2,16 +2,18 @@ package testjava;
 
 public class test {
 
-//	int型配列を受け取り、
-//	配列内の要素全てに１を加えるメソッド
-  public static void incArray(int[] array) {
-  	for (int i = 0; i < array.length; i++) {
-  		array[i]++;
+//	int型配列を作成して戻すメソッド
+  public static int[] makeArray(int size) {
+  	int[] newArray = new int[size];
+  	for(int i = 0; i < newArray.length; i++) {
+  		newArray[i] = i;
   	}
+//  	配列を戻す
+  return newArray;
   }
   public static void main(String[] args) {
-  	int[] array = {1, 2, 3};
-  	incArray(array);
+  	int[] array = makeArray(3);
+//  	arrayの全要素を出力
   	for (int i : array) {
   		System.out.println(i);
   	}
