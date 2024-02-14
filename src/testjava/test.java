5e8b9d53a4953c445d8a854f849051b5ee65d6b5
tsujita-yurika <143866084+tsujita-yurika@users.noreply.github.com>
2024-02-14 11:55:06 +0900

    自己紹介メソッド

--- src/testjava/test.java
+++ src/testjava/test.java
@@ -2,20 +2,20 @@ package testjava;
 
 public class test {
 
-//	int型配列を作成して戻すメソッド
-  public static int[] makeArray(int size) {
-  	int[] newArray = new int[size];
-  	for(int i = 0; i < newArray.length; i++) {
-  		newArray[i] = i;
+	public static void main(String[] args) {
+		introduceOneself();
+	}
+	
+//	自己紹介メソッド
+	public static void introduceOneself() {
+//		文字列、整数、浮動小数点、1文字
+  	String name = "Minato";
+  	int age = 22;
+  	double height = 169.9;
+  	char zodiac = '辰';
+  		System.out.println("私の名前は" + name + "です");
+  		System.out.println("歳は" + age + "です");
+  		System.out.println("身長は" + height + "です");
+  		System.out.println("干支は" + zodiac + "です");
   	}
-//  	配列を戻す
-  return newArray;
   }
-  public static void main(String[] args) {
-  	int[] array = makeArray(3);
-//  	arrayの全要素を出力
-  	for (int i : array) {
-  		System.out.println(i);
-  	}
-  }
-}
