4f3c73c74bb62e4a90af26d1831f31aaec86b96d
tsujita-yurika <143866084+tsujita-yurika@users.noreply.github.com>
2024-02-14 12:06:35 +0900

    メールメソッド

--- src/testjava/test.java
+++ src/testjava/test.java
@@ -3,19 +3,15 @@ package testjava;
 public class test {
 
 	public static void main(String[] args) {
-		introduceOneself();
+		String title = "お誘い";
+  	String address = "uso800@xxxx.com";
+  	String text = "今度、飲みに行きませんか";
+  	email(title, address, text);
 	}
-	
-//	自己紹介メソッド
-	public static void introduceOneself() {
-//		文字列、整数、浮動小数点、1文字
-  	String name = "Minato";
-  	int age = 22;
-  	double height = 169.9;
-  	char zodiac = '辰';
-  		System.out.println("私の名前は" + name + "です");
-  		System.out.println("歳は" + age + "です");
-  		System.out.println("身長は" + height + "です");
-  		System.out.println("干支は" + zodiac + "です");
+  	
+	public static void email(String title,String address, String text) { 
+  		System.out.println(address + "　に、以下のメールを送信しました");
+  		System.out.println("件名：" + title);
+  		System.out.println("本文：" + text);
   	}
   }
