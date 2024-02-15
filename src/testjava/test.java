03b4c1551ea1f2351990494b775476f8d3278652
tsujita-yurika <143866084+tsujita-yurika@users.noreply.github.com>
2024-02-15 18:17:10 +0900

    復習２

--- src/testjava/test.java
+++ src/testjava/test.java
@@ -3,10 +3,9 @@ package testjava;
 public class test {
 
 	public static void main(String[] args) {
-		boolean result = true;
-		char favoriteCharacter = '駆';
-		double pi = 3.14;
-		long number = 31410000000000L;
-		String msg = "ミナトの攻撃！敵に１５ポイントのダメージを与えた。";
+		int x = 5;
+		int y = 10;
+		String ans = "x+yは" + (x + y);
+		System.out.println(ans);
 	}
 }
