2bd734fdb5903833480b03d7dbeb56532d3ee553
tsujita-yurika <143866084+tsujita-yurika@users.noreply.github.com>
2024-02-15 15:16:25 +0900

    復習

--- src/testjava/test.java
+++ src/testjava/test.java
@@ -3,19 +3,9 @@ package testjava;
 public class test {
 
 	public static void main(String[] args) {
-		double triangleArea = calcTriangleArea(10.0, 5.0);
-		System.out.println("三角形の面積：" + triangleArea + "平方cm");
-		double circleArea = calcCircleArea(5.0);
-		System.out.println("円の面積：" + circleArea + "平方cm");
-	}
-
-	public static double calcTriangleArea(double bottom, double height) {
-		double area = (bottom * height) / 2;
-		return area;
-	}
-
-	public static double calcCircleArea(double radius) {
-		double area = radius * radius * 3.14;
-		return area;
+		int a = 3;
+		int b = 5;
+		int c = a * b;
+		System.out.println("縦幅3横幅5の長方形の面積は、" + c);
 	}
 }
