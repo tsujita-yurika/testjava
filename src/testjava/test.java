25b40e0c21e4a36438fbca06e39b7396ff91e818
tsujita-yurika <143866084+tsujita-yurika@users.noreply.github.com>
2024-02-15 17:58:08 +0900

    型

--- src/testjava/test.java
+++ src/testjava/test.java
@@ -3,9 +3,10 @@ package testjava;
 public class test {
 
 	public static void main(String[] args) {
-		int a = 3;
-		int b = 5;
-		int c = a * b;
-		System.out.println("縦幅3横幅5の長方形の面積は、" + c);
+		boolean result = true;
+		char favoriteCharacter = '駆';
+		double pi = 3.14;
+		long number = 31410000000000L;
+		String msg = "ミナトの攻撃！敵に１５ポイントのダメージを与えた。";
 	}
 }
