b05470f0f0b454f872e1728f801b2bb1dc904453
tsujita-yurika <143866084+tsujita-yurika@users.noreply.github.com>
2024-02-16 20:14:48 +0900

    文字列になる

--- src/testjava/test.java
+++ src/testjava/test.java
@@ -5,7 +5,7 @@ public class test {
 	public static void main(String[] args) {
 		int x = 5;
 		int y = 10;
-		String ans = "x+yは" + (x + y);
+		String ans = "x+yは" + x + y;
 		System.out.println(ans);
 	}
 }
