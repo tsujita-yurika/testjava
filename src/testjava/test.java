fd53e406c7f4b18aacf74dcedad4a8f4df548dcf
tsujita-yurika <143866084+tsujita-yurika@users.noreply.github.com>
2024-02-18 21:45:46 +0900

    存在しないインデックスを指定した場合

--- src/testjava/test.java
+++ src/testjava/test.java
@@ -5,12 +5,6 @@ public class test {
 	public static void main(String[] args) {
 		int[] num = new int[2];
 
-	// 要素に値を代入する
-	num[0] = 10;
-	num[1] = 8;
-
-	// 要素に格納された値を参照する
-	System.out.println(num[0]);  // 10
-	System.out.println(num[1]);  // 8
+		num[2] = 14;
 	}
 }
