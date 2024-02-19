124e1674c9de0d3702e623d203c9667de03ab455
tsujita-yurika <143866084+tsujita-yurika@users.noreply.github.com>
2024-02-19 21:14:22 +0900

    インデックスを変数で指定

--- src/testjava/test.java
+++ src/testjava/test.java
@@ -3,8 +3,14 @@ package testjava;
 public class test {
 
 	public static void main(String[] args) {
-		int[] num = new int[2];
+		int[] num = new int[3];
 
-		num[2] = 14;
+		num[0] = 10;
+		num[1] = 8;
+		num[2] = 16;
+
+		for (int i = 0; i < 3; i++){
+		  System.out.println(num[i]);
+		}
 	}
 }
