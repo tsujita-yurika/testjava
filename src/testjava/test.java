0c9028ab2aeaf048bce3257610147fc2636c4a7a
tsujita-yurika <143866084+tsujita-yurika@users.noreply.github.com>
2024-02-21 09:02:55 +0900

    新しい文字列への参照が変数に代入

--- src/testjava/test.java
+++ src/testjava/test.java
@@ -6,7 +6,10 @@ public class test {
 		String msg1 = "こんにちは";
 		String msg2 = msg1;
 
-		System.out.println(msg1);  // "こんにちは"
-		System.out.println(msg2);  // "こんにちは"
+		msg2 = "さようなら";
+		msg2 = "おはよう";
+		
+		System.out.println(msg1);
+		System.out.println(msg2);
 		}
 	}
