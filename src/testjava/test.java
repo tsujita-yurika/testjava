4e7a6c93f50fc2803d438966d19419422e57a625
tsujita-yurika <143866084+tsujita-yurika@users.noreply.github.com>
2024-02-25 16:17:51 +0900

    引数とメソッド

--- src/testjava/test.java
+++ src/testjava/test.java
@@ -3,22 +3,21 @@ package testjava;
 public class test {
 
 	public static void main(String[] args) {
-	  int num1, num2;
+    int num1, num2;
 
-	  num1 = 10;
-	  num2 = 8;
-	  keisan(num1, num2);
+    num1 = 10;
+    num2 = 8;
+    keisan(num1, num2);
 
-	  num1 = 23;
-	  num2 = 7;
-	  keisan(num1, num2);
-	}
+    num1 = 23;
+    keisan(num1, 7);
 
-	private static void keisan(int n1, int n2){
-	  System.out.println("num1 = " + n1 + ",num2 = " + n2);
-	  System.out.println("num1 + num2 = " + (n1 + n2));
-	  System.out.println("num1 - num2 = " + (n1 - n2));
-	  System.out.println("num1 * num2 = " + (n1 * n2));
-	  System.out.println("num1 / num2 = " + (n1 / n2));
+    keisan(15, 22);
+  }
+
+  private static void keisan(int n1, int n2){
+    System.out.println("num1 = " + n1 + ",num2 = " + n2);
+    System.out.println("num1 + num2 = " + (n1 + n2));
+    System.out.println("num1 - num2 = " + (n1 - n2));
 		}
 	}
